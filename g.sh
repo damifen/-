a483644281792816b108fde2ce9b5f36e6cd4c19
damifen <66149911+damifen@users.noreply.github.com>
2020-05-30 10:06:13 +0800

    Create g.sh

---
+++ g.sh
@@ -0,0 +1,5 @@
+sudo -i
+bash <(wget -qO- https://git.io/gclone.sh)
+git clone https://github.com/damifen/wtf
+mv /root/wtf/rclone.conf /root/.config/rclone/rclone.conf
+unzip -o /root/wtf/2400.zip -d /root/sb
