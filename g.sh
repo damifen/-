8a1223e1f8cd4f7a15cfa50b461c1e0e6885ca68
damifen <66149911+damifen@users.noreply.github.com>
2020-06-01 15:42:28 +0800

    Update g.sh

--- g.sh
+++ g.sh
@@ -3,3 +3,4 @@ bash <(wget -qO- https://git.io/gclone.sh)
 git clone https://github.com/damifen/wtf
 mv /root/wtf/rclone.conf /root/.config/rclone/rclone.conf
 unzip -o /root/wtf/2400.zip -d /root/sb
+rm -f /root/wtf/2400.zip
